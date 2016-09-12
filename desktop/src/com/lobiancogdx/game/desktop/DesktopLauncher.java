39d0b5b08a5c99a7c0193489d8fba64f64f2da9a
lobiancj <lobiancj@gmail.com>
2016-09-11 21:29:17 -0400

    Added all code from Chapter 3

--- desktop/src/com/lobiancogdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/lobiancogdx/game/desktop/DesktopLauncher.java
@@ -1,12 +1,17 @@
 package com.lobiancogdx.game.desktop;
+/**
+ * @author Jason LoBianco
+ */
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.lobiancogdx.game.LoBiancoGdxGame;
+import com.lobiancogdx.game.CanyonBunnyMain;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
+public class DesktopLauncher 
+{
+	public static void main (String[] arg) 
+	{
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new LoBiancoGdxGame(), config);
+		new LwjglApplication(new CanyonBunnyMain(), config);
 	}
 }
